e3762ce0bf8cabba9771d5a8cc31b455f255bd5b
Mateusz Starakiewicz <ms.starakiewicz@gmail.com>
2019-03-15 13:57:20 +0100

    add gitignore

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+var EventEmitter = require('events').EventEmitter;
+var OSinfo = require('./modules/OSinfo');
+
+var emitter = new EventEmitter();
+emitter.on('beforeCommand', function(instruction) {
+    console.log('You wrote: ' + instruction + ' trying to run command.')
+});
+emitter.on('afterCommand', function() {
+console.log('Finished command');
+});
+
 process.stdin.setEncoding('utf-8');
 process.stdin.on('readable', function() {
-    // metoda .read() ma za zadanie odczytać co użytkownik podał na wejściu
     var input = process.stdin.read();
     if (input !== null) {
         var instruction = input.toString().trim();
@@ -17,12 +27,13 @@ process.stdin.on('readable', function() {
 		    	console.log(process.env);
 		    	process.exit();
 		    	break;
-		    case '/getOSinfo':
-		    	var OSinfo = require('./modules/OSinfo');
+		    case '/getOSinfo':		    	
     			OSinfo.print();
 				break;
     		default: 
     			process.stderr.write('Wrong instruction!');
-		}
+		};
+		// emitowanie zdarzenia afterCommand (bez parametru)
+        emitter.emit('afterCommand');
     }
 });
